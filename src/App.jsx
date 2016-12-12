be0cfa2b1a0448b5d916532685ecc3a888d55326
Aleks <info@aleks.bz>
2016-12-12 11:19:18 +0200

    Router and Redux added

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
+
+import MainLayout from './view/layouts/MainLayout';
+import Error404 from './view/errors/Error404';
+import { Search } from './view/root/Search/Search';
+import './css/App.css';
+
+export  default class App extends Component {
+    render() {
+        return (
+            <Router history={browserHistory}>
+                <Route path="/" component={MainLayout}>
+                    <IndexRoute component={Search} />
+                    <Route path="*" component={Error404}/>
+                </Route>
+            </Router>
+        );
+    }
+}
+
+
+
+/*
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+            <Search />
+        </div>
+        <div className="container" style={{paddingTop: '20px'}}>
+            <div className="row">
+                <Contact />
+                <Contact />
+            </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
+*/
+
+
--- src/view/root/Search/Search.jsx
+++ src/view/root/Search/Search.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
-
 import Autosuggest from 'react-autosuggest';
+import Contact from './Contact';
 
 const testData = [
     'javascript',
@@ -9,48 +8,8 @@ const testData = [
     'java',
     'python'
 ];
-class Contact extends Component {
-    render() {
-        return (
-                <div className="col-xs-12 col-sm-6 col-md-6">
-                    <div className="well well-sm">
-                        <div className="row">
-                            <div className="col-sm-6 col-md-4">
-                                <img src="http://placehold.it/380x500" alt="" className="img-rounded img-responsive" />
-                            </div>
-                            <div className="col-sm-6 col-md-8">
-                                <h4>
-                                    Bhaumik Patel</h4>
-                                <small><cite title="San Francisco, USA">San Francisco, USA <i className="glyphicon glyphicon-map-marker">
-                                </i></cite></small>
-                                <p>
-                                    <i className="glyphicon glyphicon-envelope"></i>email@example.com
-                                    <br />
-                                    <i className="glyphicon glyphicon-globe"></i><a href="http://www.jquery2dotnet.com">www.jquery2dotnet.com</a>
-                                    <br />
-                                    <i className="glyphicon glyphicon-gift"></i>June 02, 1988</p>
-                                <div className="btn-group">
-                                    <button type="button" className="btn btn-primary">
-                                        Social</button>
-                                    <button type="button" className="btn btn-primary dropdown-toggle" data-toggle="dropdown">
-                                        <span className="caret"></span><span className="sr-only">Social</span>
-                                    </button>
-                                    <ul className="dropdown-menu" role="menu">
-                                        <li><a href="#">Twitter</a></li>
-                                        <li><a href="https://plus.google.com/+Jquery2dotnet/posts">Google +</a></li>
-                                        <li><a href="https://www.facebook.com/jquery2dotnet">Facebook</a></li>
-                                        <li className="divider"></li>
-                                        <li><a href="#">Github</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-        );
-    }
-}
-class Search extends Component {
+
+class SearchBar extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -158,25 +117,24 @@ class Search extends Component {
             </div>
         );
     }
+};
 
-}
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-            <Search />
-        </div>
-        <div className="container" style={{paddingTop: '20px'}}>
-            <div className="row">
-                <Contact />
-                <Contact />
+class Search extends Component {
+    render() {
+        return (
+            <div>
+                <div className="App-header">
+                    <SearchBar />
+                </div>
+                <div className="container" style={{paddingTop: '20px'}}>
+                    <div className="row">
+                        <Contact />
+                        <Contact />
+                    </div>
+                </div>
             </div>
-        </div>
-      </div>
-    );
-  }
-}
+        );
+    }
+};
 
-export default App;
+export { Search, SearchBar }
